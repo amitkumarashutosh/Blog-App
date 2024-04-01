bcf724ffcd380f495f4fd04065f5667cadb336a8
Amit <amitkumarashutos@gmail.com>
2024-04-01 20:22:57 +0530

    about and project section added

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 const Projects = () => {
   return (
-    <div>
-      <p>projects</p>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            Projects
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>This section available soon.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
